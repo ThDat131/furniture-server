5f8f3753eb9d8d8478205d9e2393ff9f8ca25b9f
nghianek123 <2051052089nghia@ou.edu.vn>
2024-09-11 18:02:57 +0700

    Revert "Merge branch 'main' of https://github.com/ThDat131/furniture-server into another-dev"
    This reverts commit 5a423f04fccd5639e1b7892083f0072f9f66e411, reversing
    changes made to fff32552be74cdb368b6ee7ddf727fccc9df6b69.

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ApiResponse } from '../interfaces/api-response.interface';
+import { isArray } from 'class-validator';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
@@ -22,6 +23,8 @@ export class HttpExceptionFilter implements ExceptionFilter {
             success: false,
             error: exception.message,
         };
+
+        console.log(exception);
 
         response.status(status).json(errorResponse);
     }
