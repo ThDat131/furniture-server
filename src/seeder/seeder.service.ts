79e96ebc7ad37d545b958844f5ddeef0de340b71
ThDat131 <truongthanhdat2002@gmail.com>
2024-10-07 20:23:07 +0700

    Add docker

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AuthService } from 'src/auth/auth.service';
+import { Banner } from 'src/schemas/banner.schema';
 import { Category } from 'src/schemas/category.schema';
+import { Product } from 'src/schemas/product.schema';
 import { User } from 'src/schemas/user.schema';
 
 @Injectable()
@@ -11,12 +13,17 @@ export class SeederService {
         @InjectModel(User.name) private readonly userModel: Model<User>,
         @InjectModel(Category.name)
         private readonly categoryModel: Model<Category>,
-        private authService: AuthService,
+        private readonly authService: AuthService,
+        @InjectModel(Banner.name) private readonly bannerModel: Model<Banner>,
+        @InjectModel(Product.name)
+        private readonly productModel: Model<Product>,
     ) {}
 
     async seed() {
         await this.createUsers();
+        await this.createBanners();
         await this.createCategories();
+        await this.createProducts();
     }
 
     private async createCategories() {
@@ -84,6 +91,120 @@ export class SeederService {
 
                 this.userModel.create(user);
             }
+        }
+    }
+
+    private async createBanners() {
+        const banners = [
+            {
+                _id: '66dd77791633f06f3690e055',
+                name: 'homeBannerImg',
+                image: {
+                    id: 'furniture/images/fmkhxtsnjhq78r4ro2hc',
+                    url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725887284/furniture/images/fmkhxtsnjhq78r4ro2hc.jpg',
+                },
+            },
+            {
+                _id: '66dfc75f8fc18bf2bbda03dc',
+                name: 'recruitmentBannerImg',
+                image: {
+                    id: 'furniture/images/p75ezsci3bxm2bumg3tx',
+                    url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725943365/furniture/images/p75ezsci3bxm2bumg3tx.jpg',
+                },
+            },
+            {
+                _id: '66dfc8368a5cc08c883fb212',
+                name: 'policyBannerImg',
+                image: {
+                    id: 'furniture/images/xootrg3o5tuf699gwavp',
+                    url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725946233/furniture/images/xootrg3o5tuf699gwavp.jpg',
+                },
+            },
+        ];
+
+        console.log(banners);
+
+        for (const banner of banners) {
+            const existingBanner = await this.bannerModel.findOne({
+                name: banner.name,
+            });
+
+            console.log(135, existingBanner);
+
+            if (!existingBanner) this.bannerModel.create(banner);
+        }
+    }
+
+    private async createProducts() {
+        const products = [
+            {
+                _id: '66d4eba822ceaa540da11ee3',
+                name: 'Đèn năng lượng',
+                description: 'Đèn năng lượng mặt trời',
+                images: [
+                    {
+                        id: 'furniture/images/tucjtguaz1w0ozrxzrhq',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725230156/furniture/images/tucjtguaz1w0ozrxzrhq.jpg',
+                    },
+                ],
+                price: 99998,
+                stock: 100,
+                categoryId: '66d83be4cdc651edcb87be30',
+                isNew: true,
+                isPotential: true,
+            },
+            {
+                _id: '66daedfee95736c3b11b850d',
+
+                name: 'Ghế dựa',
+                description: 'Ghế đọc sách',
+                images: [
+                    {
+                        id: 'furniture/images/f0mft8j6s4xz1x73qkra',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725623792/furniture/images/f0mft8j6s4xz1x73qkra.jpg',
+                    },
+                    {
+                        id: 'furniture/images/thnygfxya9qjtndhdozb',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725627976/furniture/images/thnygfxya9qjtndhdozb.jpg',
+                    },
+                ],
+                price: 600000,
+                stock: 97,
+                categoryId: '66d83bef3512bbddbf74403d',
+                isNew: false,
+                isPotential: true,
+            },
+            {
+                name: 'Bàn làm việc',
+                description: 'Kích thước 150x150',
+                images: [
+                    {
+                        id: 'furniture/images/etd2t5cclyufghmqbgrd',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725628112/furniture/images/etd2t5cclyufghmqbgrd.jpg',
+                    },
+                    {
+                        id: 'furniture/images/mb5dpvqofw3ldomkpkji',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725628132/furniture/images/mb5dpvqofw3ldomkpkji.jpg',
+                    },
+                    {
+                        id: 'furniture/images/iy2m9iaitktvbqsku2jn',
+                        url: 'http://res.cloudinary.com/dgyytgkae/image/upload/v1725628149/furniture/images/iy2m9iaitktvbqsku2jn.jpg',
+                    },
+                ],
+                price: 1000000,
+                stock: 100,
+                categoryId: '66d83bf94618e07a034d6e8c',
+                isNew: false,
+                isPotential: true,
+            },
+        ];
+
+        for (const product of products) {
+            const existingProduct = await this.productModel.findOne({
+                name: product.name,
+            });
+
+            if (!existingProduct) this.productModel.create(product);
         }
     }
 }
